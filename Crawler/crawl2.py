36503d2a6992a3b329038a88c7069ec5f130322a
machenity <machenity@gmail.com>
2017-09-08 21:49:11 +0900

    update crawler code && crawled data

--- Crawler/crawl2.py
+++ Crawler/crawl2.py
@@ -1,62 +1,39 @@
 import urllib.request
 from bs4 import BeautifulSoup
-# import win32com.client
+from pandas import DataFrame
+import pandas as pd
 
 NBA_result = [[0]*14 for i in range(30)]
 team_value = 0
 attr_value = 0
 nba_teams = "bos bkn cle cha gs chi dal den det hou ind lac lal mem mia mil min no ny okc orl phi phx por sac sa tor utah wsh".split(" ")
 
-"""
-excel = win32com.client.Dispatch("Excel.Application")
-wb = excel.Workbooks.open("C:/Users/user/Documents/NBA_Result_Predictor/Crawler/Data.xlsx")
-ws = wb.ActiveSheet
-"""
-#req = urllib.request.Request('http://www.espn.com/nba/standings')
-'''
-req = ["http://www.espn.com/nba/team/stats/_/name/{}".format(t) for t in nba_teams]
-data = urllib.request.urlopen(req).read()
-soup = BeautifulSoup(data, 'html.parser')
+dfs = []
+n = 0
 
-#teams = soup.select('#main-container > div > section > div.tab-content > div > div > table > tr > td')
-players = soup.select(".tablehead > tr > td")
-'''
 for team in nba_teams:
     req = "http://www.espn.com/nba/team/stats/_/name/{}".format(team)
     data = urllib.request.urlopen(req).read()
     soup = BeautifulSoup(data, 'html.parser')
-    players = soup.select(".tablehead > tr > td")
+    players = soup.select(".tablehead > tr")
+
+    dfn = []
 
     for player in players:
-        print(type(player.text))
+        if player['class'][0] == 'stathead':
+            continue
 
-'''
-for team in teams:
-    attr = team.text
-    if attr_value == 0 and attr[-1].isdigit() == False and attr[-1] != '-':
-        if(attr[0].isdigit() == True):
-            NBA_result[team_value][attr_value] = attr[1:]
+        elem = player.find_all('td')
 
-        else:
-            NBA_result[team_value][attr_value] = attr
+        if player['class'][0] == 'colhead':
+            head = [nm.text for nm in elem]
+            continue
 
-        attr_value += 1
+        if player['class'][0] == 'total':
+            break
 
-    else:
-            if attr.find('-') > 0:
-                i = attr.find('-')
-                attr = attr[0:i] + ',' + attr[i+1:]
-            NBA_result[team_value][attr_value] = attr
-            if attr_value != 13:
-                attr_value += 1
-            else:
-                attr_value = 0
-                team_value += 1
+        dfn.append([nm.text for nm in elem])
 
-for x in range(30):
-    for y in range(14):
-        ws.Cells(x+2,y+1).Value = NBA_result[x][y]
-
-wb.Save()
-excel.Quit()
-'''
+    df = DataFrame(dfn, columns=head)
+    df.to_csv("..\\Data\\{}_stats.csv".format(team), index=False)
+    dfs.append(df)
